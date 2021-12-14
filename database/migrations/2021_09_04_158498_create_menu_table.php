f57718eaa40d99b028d0ccef1c816acca8be43e6
carlosseda <carlossedagambin@gmail.com>
2021-12-14 20:40:46 +0100

    menu migration

---
+++ database/migrations/2021_09_04_158498_create_menu_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_menu', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('menu_name');
+            $table->string('language');
+            $table->string('link_name');
+            $table->string('description')->nullable();
+            $table->string('custom_url')->nullable();
+            $table->integer('private')->nullable();
+            $table->integer('order')->default(0);
+            $table->integer('menu_id')->unsigned();
+            $table->integer('locale_seo_id')->nullable(); 
+            $table->integer('locale_slug_seo_id')->nullable(); 
+            $table->integer('parent_id')->nullable(); 
+            $table->timestamps();
+        });
+
+        Schema::table('t_menu_item', function($table) {
+            $table->foreign('menu_id')->references('id')->on('t_menu');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_menu');
+    }
+}
